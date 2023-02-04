c8d81e225453484e71ed9be37977a0c6035e3fea
unknown <nitishrawat009@gmail.com>
2023-02-04 11:15:46 +0530

    fetching the entire table data

--- scraper.py
+++ scraper.py
@@ -6,5 +6,11 @@ url = 'https://www.skysports.com/premier-league-table'
 rawData = requests.get(url)
 soup = BeautifulSoup(rawData.text, 'html.parser')
 
-league_table = soup.find("table", class_ = "standing-table__table callfn")
-print(league_table)
+league_table = soup.find('table', class_ = 'standing-table__table callfn')
+
+for team in league_table.find_all('tbody'):
+    rows = team.find_all('tr')
+    for row in rows:
+        row_data = row.find_all('td',  class_ = 'standing-table__cell')
+        for data in row_data:
+            print(data.text.strip())
